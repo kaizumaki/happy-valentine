94b361ddc91f13d56e3aa6f6a1df4681fcb3d85a
Kazuki Imamura <kaizumaki@gmail.com>
2019-01-16 21:41:59 +0900

    インターバル調整用

--- analysis_interval.py
+++ analysis_interval.py
@@ -7,11 +7,9 @@ from dotenv import load_dotenv
 import unicodedata
 from sklearn.feature_extraction.text import TfidfVectorizer
 from operator import itemgetter
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 import csv
 import json
-
-JST = timezone(timedelta(hours=+9), 'JST')
 
 load_dotenv()
 
@@ -44,15 +42,14 @@ def get_data():
     return data
 
 
-def get_mecabed_data():
+def get_mecabed_data(previous_time, current_time):
     try:
         cnx = mysql.connector.connect(**config)
 
         if cnx.is_connected():
             cursor = cnx.cursor()
-            # query = "SELECT group_concat(t1.mecabed_data separator ' ') FROM tweet_analysis t1 JOIN valentine_tweet t2 ON t1.tweet_id = t2.id WHERE t2.mecabed IS NOT True AND t1.part <> '動詞'"
-            query = "SELECT group_concat(t1.mecabed_data separator ' ') FROM tweet_analysis t1 JOIN valentine_tweet t2 ON t1.tweet_id = t2.id WHERE t1.part <> '動詞'"
-            cursor.execute(query)
+            query = "SELECT group_concat(t1.mecabed_data separator ' ') FROM tweet_analysis t1 JOIN valentine_tweet t2 ON t1.tweet_id = t2.id WHERE t2.created_at BETWEEN %s AND %s AND t1.part <> '動詞'"
+            cursor.execute(query, (previous_time, current_time))
             data = cursor.fetchall()
 
     except errorcode as e:
@@ -120,6 +117,60 @@ def mecab_analysis(sentence):
     return result_dict
 
 
+def vectorizer_analysis(previous_time, interval_seconds):
+    now = datetime.now() + timedelta(hours=9)
+    previous = datetime.strptime(previous_time, "%Y-%m-%d %H:%M:%S")
+    current_time = previous + timedelta(seconds=interval_seconds)
+    while current_time < now:
+        mecabed_data = get_mecabed_data(previous_time, current_time)
+        if mecabed_data[0] is not None:
+            vectorizer = TfidfVectorizer(stop_words=['バレンタイン', '拡散希望', 'https', 'retweet'])
+            tfidf_matrix = vectorizer.fit_transform(mecabed_data[0])
+            feature_names = vectorizer.get_feature_names()
+            doc = 0
+            feature_index = tfidf_matrix[doc, :].nonzero()[1]
+            tfidf_scores = zip(feature_index, [tfidf_matrix[doc, x] for x in feature_index])
+            scored_words = [(feature_names[i], s) for (i, s) in tfidf_scores]
+
+            current = current_time.strftime("%Y-%m-%d-%H-%M-%S")
+
+            csv_file_name = 'data/' + current + '.csv'
+            json_file_name = 'data/' + current + '.json'
+            with open(csv_file_name, 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerows(sorted(scored_words, key=itemgetter(1), reverse=True))
+
+            csvfile = open(csv_file_name, 'r')
+            jsonfile = open(json_file_name, 'w')
+
+            fieldnames = ('word', 'score')
+            reader = csv.DictReader(csvfile, fieldnames)
+
+            jsonfile.write('{"scored_words":[')
+            for i, row in enumerate(reader):
+                if i != 0:
+                    jsonfile.write(',\n')
+                json.dump(row, jsonfile, ensure_ascii=False)
+            jsonfile.write(']}')
+
+            with open('data_names.csv', 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow([current])
+
+            data_names_csvfile = open('data_names.csv', 'r')
+            data_names_jsonfile = open('data_names.json', 'w')
+            data_names_reader = csv.DictReader(data_names_csvfile, ('filename',))
+
+            data_names_jsonfile.write('[')
+            for i, row in enumerate(data_names_reader):
+                if i != 0:
+                    data_names_jsonfile.write(',\n')
+                json.dump(row, data_names_jsonfile, ensure_ascii=False)
+            data_names_jsonfile.write(']')
+
+            current_time = current_time + timedelta(seconds=interval_seconds)
+
+
 if __name__ == "__main__":
     data = get_data()
     for row in data:
@@ -151,52 +202,7 @@ if __name__ == "__main__":
                 adverb_data = ' '.join(adverb_list)
                 insert_data(row['id'], '副詞', adverb_data)
 
-    mecabed_data = get_mecabed_data()
-    if mecabed_data[0] is not None:
-        vectorizer = TfidfVectorizer(stop_words=['バレンタイン', '拡散希望', 'https', 'retweet'])
-        tfidf_matrix = vectorizer.fit_transform(mecabed_data[0])
-        feature_names = vectorizer.get_feature_names()
-        doc = 0
-        feature_index = tfidf_matrix[doc, :].nonzero()[1]
-        tfidf_scores = zip(feature_index, [tfidf_matrix[doc, x] for x in feature_index])
-        scored_words = [(feature_names[i], s) for (i, s) in tfidf_scores]
+        mecabed = True
+        update_data(row['id'], mecabed)
 
-        now = datetime.now(JST).strftime("%Y-%m-%d-%H-%M-%S")
-
-        csv_file_name = 'data/' + now + '.csv'
-        json_file_name = 'data/' + now + '.json'
-        with open(csv_file_name, 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerows(sorted(scored_words, key=itemgetter(1), reverse=True))
-
-        csvfile = open(csv_file_name, 'r')
-        jsonfile = open(json_file_name, 'w')
-
-        fieldnames = ('word', 'score')
-        reader = csv.DictReader(csvfile, fieldnames)
-
-        jsonfile.write('{"scored_words":[')
-        for i, row in enumerate(reader):
-            if i != 0:
-                jsonfile.write(',\n')
-            json.dump(row, jsonfile, ensure_ascii=False)
-        jsonfile.write(']}')
-
-        with open('data_names.csv', 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow([now])
-
-        data_names_csvfile = open('data_names.csv', 'r')
-        data_names_jsonfile = open('data_names.json', 'w')
-        data_names_reader = csv.DictReader(data_names_csvfile, ('filename',))
-
-        data_names_jsonfile.write('[')
-        for i, row in enumerate(data_names_reader):
-            if i != 0:
-                data_names_jsonfile.write(',\n')
-            json.dump(row, data_names_jsonfile, ensure_ascii=False)
-        data_names_jsonfile.write(']')
-
-        for row in data:
-            mecabed = True
-            update_data(row['id'], mecabed)
+    vectorizer_analysis('2019-01-16 20:15:27', 60*30)
