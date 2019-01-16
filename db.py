85b4ea175c0792ca77de106ebc52a4a7d22ffc24
Kazuki Imamura <kaizumaki@gmail.com>
2019-01-16 20:21:00 +0900

    datetime型に修正

--- db.py
+++ db.py
@@ -12,7 +12,7 @@ TABLES['valentine_tweet'] = (
     "CREATE TABLE `valentine_tweet` ("
     "  `id` int(11) NOT NULL AUTO_INCREMENT,"
     "  `username` varchar(255) NOT NULL,"
-    "  `created_at` date NOT NULL,"
+    "  `created_at` datetime NOT NULL,"
     "  `tweet` text NOT NULL,"
     "  `wakachi` text,"
     "  `mecabed` enum('True','False'),"
